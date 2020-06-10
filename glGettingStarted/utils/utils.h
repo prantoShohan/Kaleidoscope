3cfdc1f812d5ba543ece977514c59b4fe05d6c89
prantoShohan <me.pranto@gmail.com>
2020-06-10 18:53:30 +0600

    Project Setup

---
+++ glGettingStarted/utils/utils.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "glad/glad.h" 
+#include <iostream>
+
+#define ASSERT(x) if (!(x)) __debugbreak()
+
+#define glCall(x) glClearError();\
+	x;\
+	ASSERT(glLogCall(#x, __FILE__, __LINE__))
+
+static void glClearError() {
+	while (glGetError());
+}
+
+static bool glLogCall(const char* function, const char* file, int line) {
+	while (GLenum error = glGetError()) {
+		std::cout << "[Opengl Error] (" << error << "):" << function << " " << file << ": " << line << std::endl;
+		return false;
+	}
+	return true;
+}
